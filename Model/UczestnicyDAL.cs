4eb54c9a0277c3edc240bdef2c5d8b37b94d8b3f
Michael <45467061+mdemcode@users.noreply.github.com>
2019-10-16 21:14:51 +0200

    drobne poprawki

--- Model/UczestnicyDAL.cs
+++ Model/UczestnicyDAL.cs
@@ -8,8 +8,8 @@ namespace Fundusz2.Model
             var uczestnicy = new ObservableCollection<Uczestnik>();
             if (FunduszDAL.trybProj) { 
             //if (Properties.Settings.Default._proj) {
-                uczestnicy.Add(new Uczestnik { ImieNazwisko = "MichalD", EmailTelefon = "607798", DataPrzystapienia = DateTime.Now });
-                uczestnicy.Add(new Uczestnik { ImieNazwisko = "Stasiek", EmailTelefon = "999", DataPrzystapienia = DateTime.Today.Date });
+                uczestnicy.Add(new Uczestnik { ImieNazwisko = "MichalD", EmailTelefon = "607798", DataPrzystapienia = DateTime.Today });
+                uczestnicy.Add(new Uczestnik { ImieNazwisko = "Stasiek", EmailTelefon = "999", DataPrzystapienia = DateTime.Today });
                 uczestnicy.Add(new Uczestnik { ImieNazwisko = "Krycha", EmailTelefon = "997", DataPrzystapienia = DateTime.Today });
             }
             else {
