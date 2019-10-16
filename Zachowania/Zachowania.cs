761952326239576b517aff79d8fb55efd6460655
Michael <45467061+mdemcode@users.noreply.github.com>
2019-10-16 20:03:47 +0200

    zachowania Button'ow

---
+++ Zachowania/Zachowania.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace Fundusz2.Zachowania {
+    public class Zachowania : Behavior<Window> {
+        public static readonly DependencyProperty PrzyciskProperty =
+            DependencyProperty.Register(
+                "Przycisk",
+                typeof(Button),
+                typeof(Zachowania),
+                new PropertyMetadata(null, PrzyciskZmieniony)
+            );
+        public Button Przycisk {
+            get { return (Button)GetValue(PrzyciskProperty); }
+            set { SetValue(PrzyciskProperty, value); }
+        }
+        private static void PrzyciskZmieniony(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+            Window okno = (d as Zachowania).AssociatedObject;
+            void button_Click(object sender, RoutedEventArgs _e) {
+                okno.Hide();
+                if (okno is MainWindow) Application.Current.Shutdown();
+            }
+            if (e.OldValue != null) ((Button)e.OldValue).Click -= button_Click;
+            if (e.NewValue != null) ((Button)e.NewValue).Click += button_Click;
+        }
+    }
+
+    //public class OtworzOkno : Behavior<Window> {
+
+    //    public static readonly DependencyProperty PrzyciskProperty =
+    //        DependencyProperty.Register(
+    //            "Przycisk",
+    //            typeof(Button),
+    //            typeof(OtworzOkno),
+    //            new PropertyMetadata(null, PrzyciskOtwierania)
+    //        );
+    //    public Button Przycisk {
+    //        get { return (Button)GetValue(PrzyciskProperty); }
+    //        set { SetValue(PrzyciskProperty, value); }
+    //    }
+    //    public static readonly DependencyProperty ParametrProperty =
+    //        DependencyProperty.Register(
+    //            "Parametr",
+    //            typeof(string),
+    //            typeof(OtworzOkno)
+    //        );
+    //    public string Parametr {
+    //        get { return (string)GetValue(ParametrProperty); }
+    //        set { SetValue(ParametrProperty, value); }
+    //    }
+    //    private static void PrzyciskOtwierania(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+    //        var parametr = "Fundusz2.View." + (d as OtworzOkno).Parametr;
+    //        // LUB
+    //        //var opis = "Fundusz2.View." + (d as OtworzOkno).Przycisk.Tag.ToString();
+    //        var okno = (Window)Activator.CreateInstance(Type.GetType(parametr)); //opis lub parametr
+    //        void button_Click(object sender, RoutedEventArgs _e) {
+    //            okno.ShowDialog();
+    //        }
+    //        if (e.OldValue != null) ((Button)e.OldValue).Click -= button_Click;
+    //        if (e.NewValue != null) ((Button)e.NewValue).Click += button_Click;
+    //    }
+    //}
+
+    public class OtworzOkno : Behavior<Button> {
+        public string Parametr { get; set; }
+        protected override void OnAttached() {
+            Button przycisk = this.AssociatedObject;
+            if (przycisk!=null) przycisk.Click += Przycisk_Click;
+        }
+        private void Przycisk_Click(object sender, RoutedEventArgs e) {
+            try {
+                var okno = (Window)Activator.CreateInstance(Type.GetType("Fundusz2.View." + Parametr));
+                okno.ShowDialog();
+            }
+            catch {
+                MessageBox.Show("Nie mogęo tworzyć tego okna!");
+            }
+        }
+    }
+}
