1aa76761baf766e6ed106065205026e673331930
Michael <45467061+mdemcode@users.noreply.github.com>
2019-11-07 21:48:10 +0100

    po scaleniu galezi

--- ViewModel/TestViewModel.cs
+++ ViewModel/TestViewModel.cs
@@ -2,8 +2,7 @@
 using GalaSoft.MvvmLight;
 using System.Collections.Generic;
 
-namespace Fundusz2.ViewModel
-{
+namespace Fundusz2.ViewModel {
     public class TestViewModel : ViewModelBase {
 
         #region KONSTRUKTOR
